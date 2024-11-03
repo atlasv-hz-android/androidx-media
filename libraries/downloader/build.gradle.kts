ff8515358e3ed8a0820a4f652eb4ef50e35b445f
weiping <weiping@atlasv.com>
2024-11-03 23:10:21 +0800

    build: Publish lib

--- libraries/downloader/build.gradle.kts
+++ libraries/downloader/build.gradle.kts
@@ -41,8 +41,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation(project(":lib-exoplayer"))
-    implementation(project(":lib-datasource"))
-    implementation(project(":lib-datasource-okhttp"))
+    api(project(":lib-datasource"))
+    api(libs.androidx.media3.datasource.okhttp)
+    api(libs.androidx.media3.database)
+    api(libs.androidx.media3.common)
     api("com.atlasv.android:loader:1.2.0")
 }
